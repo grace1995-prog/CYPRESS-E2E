dcfe67b60f873428c361b4dd728e64c9cc3d4611
Mfonma uko <ukomfonmamonday@gmail.com>
2024-08-06 10:24:52 +0100

    added reporting

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,14 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     chromeWebSecurity: false,
     defaultCommandTimeout: 10000,
     viewportHeight: 938,
     viewportWidth: 1520,
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
